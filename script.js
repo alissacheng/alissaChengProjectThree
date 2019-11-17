e0cb5445020cd06b1c55d80981caa6e656405ba3
Alissa <alissabc@hotmail.com>
2019-11-17 08:32:06 -0500

    fixed font

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gameApp = {};
 
 //Create a Deck of Cards
-const suits = ["♣", "♦", "♥", "♠"];
+suits = ["♣", "♦", "♥", "♠"];
 
 const ranks = [ "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
